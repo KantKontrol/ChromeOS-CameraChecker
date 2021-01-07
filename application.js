22f3d10f22f4d0375a700e90f8efb9b721eec0e5
KantKontrol <nick.derissio.tech@gmail.com>
2021-01-07 16:24:44 -0500

    added code that stops camera on appilication close

--- application.js
+++ application.js
@@ -3,7 +3,7 @@ window.onload = function() {
 var video = document.querySelector("#videoElement");
 
 if (navigator.mediaDevices.getUserMedia) {
-    
+
     navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
         video.srcObject = stream;
@@ -14,6 +14,19 @@ if (navigator.mediaDevices.getUserMedia) {
 }
 
  document.querySelector('#reset').onclick = function() {
-   window.close();
+    stopCamera(video);
+    window.close();
  };
+}
+
+function stopCamera(video){
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
+
+    for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+    }
+
+    video.srcObject = null;
 }
