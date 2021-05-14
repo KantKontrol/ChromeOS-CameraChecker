9111055b463dc3565a706f80bf3a7cea09de95ca
KantKontrol <nick.derissio.tech@gmail.com>
2021-05-14 17:42:36 -0400

    re-did the way videos are recorded and stopped

--- application.js
+++ application.js
@@ -5,22 +5,30 @@ const EventType = {
     ERROR: 'ERROR'
 }
 
+
+/*
+  hit record on media recorder -> failed to execute start on media recorder error after hitting record 3 times
+  credit text not styled well
+  Fix close and clear memory?
+*/
+
 window.onload = function() {
 
     //Recording elements
     let recordVideoEL = document.getElementById('record-element');
     let startRecordButton = document.getElementById('start-recording');
+    let stopRecordButton = document.getElementById('stop-recording');
     startRecordButton.disabled = true;
+    stopRecordButton.disabled = true;
 
     let playbackVideoEL = document.getElementById('playback-element');
-    let endRecordButton = document.getElementById('end-recording');
 
     let eventLog = document.getElementById('event-log');
-
-    let recordingTimeMS = 6000;
     //==========================================
 
     let userStream = null;
+    let mediaRecorder = null;
+    let chunks = [];
 
     const init = () => {
         navigator.mediaDevices.getUserMedia({
@@ -28,21 +36,17 @@ window.onload = function() {
             audio: true
           }).then(stream => {
             userStream = stream;
-            if(stream){
+            if(userStream){
                 startRecordButton.disabled = false;
                 recordVideoEL.srcObject = userStream;
+                mediaRecorder = new MediaRecorder(userStream);
+                setupMediaRecorderListeners();
             }
           })
           .catch(e => {
               logEvent(EventType.ERROR, "Error loading camera stream");
           });
     }
-
-
-    function wait(delayInMS) {
-        return new Promise(resolve => setTimeout(resolve, delayInMS));
-    }
-
 
     const logEvent = (type, eventStatus) => {
         let newEventEl = document.createElement('div');
@@ -61,118 +65,43 @@ window.onload = function() {
     }
 
 
-    const startRecording = (stream, lengthInMS) => {
-        let recorder = new MediaRecorder(stream);
-        let data = [];
-      
-        recorder.ondataavailable = event => data.push(event.data);
-        recorder.start();
-      
-        let stopped = new Promise((resolve, reject) => {
-          recorder.onstop = resolve;
-          recorder.onerror = event => reject(event.name);
-        });
-      
-        let recorded = wait(lengthInMS).then(
-          () => recorder.state == "recording" && recorder.stop()
-        );
-      
-        return Promise.all([
-          stopped,
-          recorded
-        ])
-        .then(() => data);
-    }
-
-    const beginRecordingStream = (stream) => {
-        recordVideoEL.srcObject = stream;
-        recordVideoEL.captureStream = recordVideoEL.captureStream;
-        logEvent(EventType.SUCCESS, "Started Recording...");
-       return new Promise(resolve => recordVideoEL.onplaying = resolve);
-    }
-
     startRecordButton.addEventListener("click", function() {
-        beginRecordingStream(userStream).then(() => startRecording(recordVideoEL.captureStream(), recordingTimeMS))
-        .then (recordedChunks => {
-          let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-          playbackVideoEL.src = URL.createObjectURL(recordedBlob);
+      if(userStream){
+        startRecordButton.disabled = true;
+        stopRecordButton.disabled = false;
+        chunks = [];
+         mediaRecorder.start();
+      }
+    });
 
-        recordVideoEL.srcObject = null;
-        logEvent(EventType.SUCCESS, "Successfully recorded video...");
-      
-        })
-        .catch(e => console.log(e));
-      }, false);
-
-      function stop(stream) {
-        stream.getTracks().forEach(track => track.stop());
-        stream = null;
+    setupMediaRecorderListeners = function(){
+      mediaRecorder.ondataavailable = ({data}) => chunks.push(data);
+      mediaRecorder.onstop = () => {
+        const blob = new Blob(chunks, { 'type' : 'video/mp4' });
+        chunks = [];
+        playbackVideoEL.src = window.URL.createObjectURL(blob);
+      }
     }
 
+    stopRecordButton.addEventListener('click', () => {
+      mediaRecorder.stop();
+      startRecordButton.disabled = false;
+      stopRecordButton.disabled = true;
+    });
 
      document.querySelector('#reset').onclick = function() {
       if(userStream !== null){
-        stop(userStream);
+        const tracks = userStream.getTracks();
+      
+        tracks.forEach(function(track) {
+          track.stop();
+        });
+      
+        recordVideoEL.srcObject = null; 
       }
         window.close();
      };
 
 
-    //   endRecordButton.addEventListener("click", function() {
-    //     stop(recordVideoEL.srcObject);
-    //   }, false);
-
-
-      init();
-
-
-    //=================================================================
-// var video = document.querySelector("#videoElement");
-
-// if (navigator.mediaDevices.getUserMedia) {
-
-//     navigator.mediaDevices.getUserMedia({ video: true, audio:true })
-//     .then(function (stream) {
-//         video.srcObject = stream;
-//         videoIsStream = true;
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-// }
-
-//  document.querySelector('#reset').onclick = function() {
-//     stopCamera(video);
-//     window.close();
-//  };
-
-//  document.querySelector('#toggleAudio').onclick = function() {
-//     let m = document.getElementById('videoElement');
-//     let audioText = document.getElementById('audioDetect');
-//     if(m.muted){
-//         m.muted = false
-//         audioText.innerHTML = "Audio is not muted";
-//         audioText.classList = "";
-//     }
-//     else {
-//         m.muted = true
-//         audioText.innerHTML = "Audio is muted";
-//         audioText.classList = "audioMutedText";
-//     } 
-//  };
-
-// }
-
-// function stopCamera(video){
-//     if(videoIsStream){
-//         var stream = video.srcObject;
-//         var tracks = stream.getTracks();
-    
-//         for (var i = 0; i < tracks.length; i++) {
-//             var track = tracks[i];
-//             track.stop();
-//         }
-    
-//         video.srcObject = null;
-//     }
+     init();
 }
